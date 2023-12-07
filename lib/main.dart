7fe8c3a8602d02ec321fcd511d473e5fd14e30c9
Brandon Trautmann <btrautmann@users.noreply.github.com>
2023-12-07 09:29:56 -0500

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,29 +8,28 @@ import 'package:provider/provider.dart';
 import 'package:signal_repro/async_value_holder.dart';
 import 'package:signals/signals_flutter.dart';
 
+// This signal is used to simulate a piece of data that can
+// change many times throughout the app's lifecycle.
 final mainSignal = signal<int>(0);
 
 void main() {
+  // Pretend to fetch new data from the server that other state in the
+  // app should respond to.
   Timer.periodic(const Duration(seconds: 5), (timer) {
     mainSignal.value = Random().nextInt(100);
   });
   runApp(const MainApp());
 }
 
-Future<DailyNetIncomeData> myFuture() async {
-  await Future<void>.delayed(const Duration(seconds: 1));
-  return DailyNetIncomeData(
-    dailyIncome: Random().nextInt(100),
-    totalIncome: Random().nextInt(100),
-    averageNetIncome: Random().nextInt(100),
-  );
-}
-
+// A simple state container that exposes a signal to be displayed on the UI
 class DataHolder {
   ReadonlySignal<AsyncValue<DailyNetIncomeData>> get data => _data.toReadonlySignal();
   final _data = signal<AsyncValue<DailyNetIncomeData>>(const AsyncLoading());
 
   DataHolder() {
+    // In lieu of something like a `StreamSignal` that automatically reevaluates
+    // in response to the `signal`s accessed within changing, run an `effect`
+    // that will update `_data` whenever `mainSignal` changes.
     _observe();
   }
 
@@ -39,6 +38,9 @@ class DataHolder {
       _data.value = const AsyncLoading();
       try {
         await Future<void>.delayed(const Duration(seconds: 1));
+        // Placing this `print` BEFORE the `await` will cause the UI to
+        // work as expected. As it is, the UI will not update after the first
+        // running of this `effect`.
         print('Effect running for ${mainSignal.value}');
         _data.value = AsyncData(
           DailyNetIncomeData(
